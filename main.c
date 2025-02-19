6f58de89a0fa7189d40122921b7b8383014dc4a4
methy-dino <gutotlima@gmail.com>
2025-02-19 00:59:08 -0300

    perfomance increase, and more readable code.

--- main.c
+++ main.c
@@ -11,6 +11,115 @@
 #include "libs/string.h"
 #include <security/pam_appl.h>
 #include <fcntl.h>
+String* configs;
+String* editor;
+String* terminal;
+String* home;
+void defaults(){
+	struct stat st = {0};
+	if (stat(home->string, &st) != 0){
+		printf("no file storage folder found, creating a new one\n");
+		mkdir(home->string, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+	}
+	configs = cloneStr(home);
+	appendPtr(configs, "/settings.txt", 9);
+	int settings;
+	if (stat(configs->string, &st) != 0) {
+		settings = open(configs->string, O_WRONLY | O_APPEND | O_CREAT, 0644);
+		printf("settings not found, initializing new settings\n");
+		write(settings, "gnome-terminal -- \nvim ", 22);
+		close(settings);
+	}
+}
+void loadSettings(){
+	FILE* file;
+    file = fopen(configs->string, "r");
+	terminal = emptyStr(64);
+	char read[128];
+	fgets(read, 128, file);
+	appendNoLen(terminal, read);
+	terminal->length--;
+	terminal->string[terminal->length] = '\0';
+	editor = emptyStr(64);
+	fgets(read, 128, file);
+	appendNoLen(editor, read);
+	fclose(file);
+}
+void listSaves(){
+	DIR* folder = opendir(home->string);
+		int currLen = 0;
+		struct dirent* currFile;
+		printf("the current saved terminals are: \n");
+		while ((currFile=readdir(folder)) != NULL){
+			if (!strcmp (currFile->d_name, "."))
+           			 continue;
+        		if (!strcmp (currFile->d_name, ".."))
+            			continue;
+			// cuts .sh out of the name and doesn't log undesired files
+			currLen = 0;
+			while (currFile->d_name[currLen] != '\0'){
+				currLen++;
+			}
+			if (currFile->d_name[currLen - 1] == 'h' && currFile->d_name[currLen - 2] == 's'){
+				currFile->d_name[currLen - 3] = '\0';
+				printf("· %s\n", currFile->d_name);
+			}
+		}
+}
+void delete(char** args, int argL){
+	// We can clone home, but since we won't use it further, it is useless.
+	//String* currFilePath = cloneStr(home);
+	appendPtr(home, "/", 1);
+	// save length for terminator shenanigans.
+	int prevL = home->length;
+	for (int i = 2; i < argL; i++){
+		appendNoLen(home, args[i]);
+		appendPtr(home, ".sh", 3);
+		if (remove(home->string) == 0){
+			printf("removed saved terminal named %s\n", args[i]);
+		} else {
+			printf("\033[31m failed to remove saved terminal named %s\n\033[0m", args[i]);
+		}
+		home->length = prevL;
+		home->string[prevL] = '\0';
+	}
+}
+void loadFiles(char** args, int argL){
+	struct stat st = {0};
+	appendPtr(home, "/", 1);	
+		String* command;
+		int i;
+		if (argL > 3 && strcmp(args[2], "local") == 0){
+			command = emptyStr(20);
+			appendNoLen(home, args[3]);
+			i = 3;
+		} else {
+			command = cloneStr(terminal);
+			appendNoLen(home, args[2]);
+			i = 2;
+		}
+		while (i < argL){
+			String* clonePath = cloneStr(home);
+			String*	cloneCommand = cloneStr(command);
+		    appendPtr(clonePath, ".sh", 3);
+			appendStr(cloneCommand, clonePath);
+			if (argL < 3){
+				printf("\033[31m unspecified terminal to load \nPROCESS ABORTED \n\033[0m");
+				fflush(stdout);
+				return;
+			}
+				//appendNoLen(command, args[2]);
+			if (stat(clonePath->string, &st) != 0){
+				printf("\033[31m invalid terminal to load \nPROCESS ABORTED \n\033[0m");
+			} else {
+					system(cloneCommand->string);
+				}
+			// ehhhh why free memory, arena is going to close right after this line anyways.
+			//discardStr(cloneCommand);
+			//discardStr(clonePath);
+			i++;
+			}
+}
 int main(int argL, char** args){
 	char* user = getlogin();
 	int userL = 0;
@@ -39,35 +148,10 @@ int main(int argL, char** args){
 	path[userL+14] = 'R';
 	path[userL+15] = 'M';
 	path[userL+16] = '\0';
-	String* str = buildStr(path, userL+16);
+	home = buildStr(path, userL+16);
+	defaults();
+	loadSettings();
 	struct stat st = {0};
-	//pam_start();
-	if (stat(str->string, &st) != 0){
-		printf("no file storage folder found, creating a new one\n");
-		mkdir(str->string, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-	}
-	String* configs = cloneStr(str);
-	appendPtr(configs, "/settings.txt", 9);
-	int settings;
-	if (stat(configs->string, &st) != 0) {
-		settings = open(configs->string, O_WRONLY | O_APPEND | O_CREAT, 0644);
-		printf("settings not found, initializing new settings\n");
-		write(settings, "gnome-terminal -- \nvim ", 22);
-		close(settings);
-	}
-	FILE* file;
-        file = fopen(configs->string, "r");
-	String* terminal = emptyStr(64);
-	char read[64];
-	fgets(read, 64, file);
-	appendNoLen(terminal, read);
-	terminal->length--;
-	terminal->string[terminal->length] = '\0';
-	String* editor = emptyStr(64);
-	char read2[64];
-	fgets(read2, 64, file);
-	appendNoLen(editor, read2);
-	fclose(file);
 	if (argL == 2 && strcmp(args[1], "clone") == 0){
 		// getcwd doesn't finish the string????
 		// data will PROBABLY BE SAFE, but just to be sure
@@ -91,107 +175,48 @@ int main(int argL, char** args){
 		system(cloner->string);
 		return 0;
 	} else if (argL > 1 && strcmp(args[1], "load") == 0){
-		appendPtr(str, "/", 1);	
-		String* command;
-		int i;
-		if (argL > 3 && strcmp(args[2], "local") == 0){
-			command = emptyStr(20);
-			appendNoLen(str, args[3]);
-			i = 3;
-		} else {
-			command = cloneStr(terminal);
-			appendNoLen(str, args[2]);
-			i = 2;
-		}
-		while (i < argL){
-			String* clonePath = cloneStr(str);
-			String*	cloneCommand = cloneStr(command);
-		       	appendPtr(clonePath, ".sh", 3);
-			appendStr(cloneCommand, clonePath);
-			if (argL < 3){
-				printf("\033[31m unspecified terminal to load \nPROCESS ABORTED \n\033[0m");
-				fflush(stdout);
+		loadFiles(args, argL);
+	} else if (argL > 1 && strcmp(args[1], "create") == 0){
+	if (argL != 3){
+		printf("\033[31m failed to parse arguments, invalid quantity \n\033[0m");
+		return 0;
+	}
+	appendPtr(home, "/", 1);
+	appendNoLen(home, args[2]);
+	appendPtr(home, ".sh", 3);
+	appendStr(editor, home);
+	if (access(home->string, F_OK) !=0){
+		printf("creating terminal template %s: \n", args[2]);
+		String* flagExec = buildStr("chmod +x ", 9);
+		appendStr(flagExec, home);
+		String* baseCode = buildStr("#/bash/sh\n",10);
+		appendPtr(baseCode, "cd ", 3);
+		char currDir[256];
+		getcwd(currDir, 256);
+		appendNoLen(baseCode, currDir);
+		appendPtr(baseCode, "\n", 1);
+		appendPtr(baseCode, "exec bash", 9);
+		int file = open(home->string, O_WRONLY | O_APPEND | O_CREAT, 0644);
+		write(file, baseCode->string, baseCode->length);
+		system(flagExec->string);
+		system(editor->string);
+	} else {
+		printf("a saved terminal with that name already exists, do you wish to edit it? (y/n)  ");
+		char input;
+		while (0==0){
+			scanf("%c", &input);
+			if ((input == 'y' || input == 'Y')){
+				system(editor->string);
+				return 0;
+			} else if ((input == 'n' || input == 'N')){
 				return 0;
 			}
-				//appendNoLen(command, args[2]);
-			if (stat(clonePath->string, &st) != 0){
-				printf("\033[31m invalid terminal to load \nPROCESS ABORTED \n\033[0m");
-			} else {
-					system(cloneCommand->string);
-				}
-			discardStr(cloneCommand);
-			discardStr(clonePath);
-			i++;
-			}
-		} else if (argL > 1 && strcmp(args[1], "create") == 0){
-		if (argL != 3){
-			printf("\033[31m failed to parse arguments, invalid quantity \n\033[0m");
-			return 0;
 		}
-		appendPtr(str, "/", 1);
-		appendNoLen(str, args[2]);
-		appendPtr(str, ".sh", 3);
-		appendStr(editor, str);
-		if (access(str->string, F_OK) !=0){
-			printf("creating terminal template %s: \n", args[2]);
-			String* flagExec = buildStr("chmod +x ", 9);
-			appendStr(flagExec, str);
-			String* baseCode = buildStr("#/bash/sh\n",10);
-			appendPtr(baseCode, "cd ", 3);
-			char currDir[256];
-			getcwd(currDir, 256);
-			appendNoLen(baseCode, currDir);
-			appendPtr(baseCode, "\n", 1);
-			appendPtr(baseCode, "exec bash", 9);
-			int file = open(str->string, O_WRONLY | O_APPEND | O_CREAT, 0644);
-			write(file, baseCode->string, baseCode->length);
-			system(flagExec->string);
-			system(editor->string);
-		} else {
-			printf("a saved terminal with that name already exists, do you wish to edit it? (y/n) ");
-			char input;
-			while (0==0){
-				scanf("%c", &input);
-				if (input == 'y' || input == 'Y'){
-					system(editor->string);
-					return 0;
-				} else if (input == 'n' || input == 'N'){
-					return 0;
-				}
-			}
-		}
+	}
 	} else if (argL > 1 && strcmp(args[1], "delete") == 0){
-		appendPtr(str, "/", 1);
-		for (int i = 2; i < argL; i++){
-			String* currFilePath = cloneStr(str);
-			appendNoLen(currFilePath, args[i]);
-			appendPtr(currFilePath, ".sh", 3);
-			if (remove(currFilePath->string) == 0){
-				printf("removed saved terminal named %s\n", args[i]);
-			} else{
-				printf("\033[31m failed to remove saved terminal named %s\n\033[0m", args[i]);
-			}
-		}
+		delete(args, argL);
 	} else if (argL > 1 && (strcmp(args[1], "list") == 0 || strcmp(args[1], "view") == 0 || strcmp(args[1], "saves") == 0)){
-		DIR* folder = opendir(str->string);
-		int currLen = 0;
-		struct dirent* currFile;
-		printf("the current saved terminals are: \n");
-		while ((currFile=readdir(folder)) != NULL){
-			if (!strcmp (currFile->d_name, "."))
-           			 continue;
-        		if (!strcmp (currFile->d_name, ".."))
-            			continue;
-			// cuts .sh out of the name and doesn't log undesired files
-			currLen = 0;
-			while (currFile->d_name[currLen] != '\0'){
-				currLen++;
-			}
-			if (currFile->d_name[currLen - 1] == 'h' && currFile->d_name[currLen - 2] == 's'){
-				currFile->d_name[currLen - 3] = '\0';
-				printf("· %s\n", currFile->d_name);
-			}
-		}
+		listSaves();
 	} else if (argL > 2 && strcmp(args[1], "editor") == 0){
 		FILE* set = fopen(configs->string, "w");
 		fwrite(terminal->string, terminal->length, 1, set);
